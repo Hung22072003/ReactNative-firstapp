fff191abf5f95ff298bb9bbce13946e9bbf5f858
Hung22072003 <tranngocnhathung2207@gmail.com>
2024-04-23 21:46:00 +0700

    add RNForms

--- App.js
+++ App.js
@@ -1,66 +1,56 @@
-import { StyleSheet, SafeAreaView, Platform, ScrollView, View, Text, StatusBar, FlatList, SectionList } from 'react-native';
-import pokemonList from "./data.json"
-import groupedPokemonList from "./grouped-data.json"
+import { StyleSheet, SafeAreaView, Platform, ScrollView, View, Text, StatusBar, TextInput, Switch, Button, Image, KeyboardAvoidingView } from 'react-native';
+import { useState } from 'react';
 export default function App() {
+
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [errors, setErrors] = useState({})
+
+  const validateForm = () => {
+    let errors = {}
+    if(!username) errors.username = "Username is required"
+    if(!password) errors.password = "Password is required"
+    setErrors(errors)
+
+    return Object.keys(errors).length === 0
+  }
+
+  const handleForm = () => {
+    if(validateForm()) {
+      console.log("Submitted", username, password)
+      setUsername("")
+      setPassword("")
+      setErrors({})
+    }
+  }
   return (
-    <SafeAreaView style = {styles.container}>
-      {/* <ScrollView style = {styles.scrollView}>
-        {
-          pokemonList.map((pokemon) => {
-            return (
-              <View style = {styles.card} key = {pokemon.id}>
-                <Text style = {styles.cardText}>{pokemon.type}</Text>
-                <Text style = {styles.cardText}>{pokemon.name}</Text>
-              </View>
-            );
-          })
-        }
-      </ScrollView> */}
-
-      <View style = {styles.scrollView}>
-        {/* <FlatList 
-          data = {pokemonList}
-          renderItem={({item}) => {
-            // console.log(item.id)
-            return (
-              <View style = {styles.card} key = {item.id}>
-                <Text style = {styles.cardText}>{item.type}</Text>
-                <Text style = {styles.cardText}>{item.name}</Text>
-              </View>
-            );
-          }}
-
-          keyExtractor={(item, index) => item.id.toString()}
-          ItemSeparatorComponent={<View style = {{height: 16}}></View>}
-          ListEmptyComponent={<Text>No items found</Text>}
-          ListHeaderComponent={<Text style = {styles.headerList}>Pokemon List</Text>}
-          ListFooterComponent={<Text style = {styles.footerList}>End of List</Text>}
-        /> */}
-
-        <SectionList 
-          sections={groupedPokemonList}
-          renderItem={({item}) => {
-            // console.log(item)
-            return (
-              <View style = {styles.card}>
-                <Text style = {styles.cardText}>{item}</Text>
-              </View>
-            )
-          }}
-
-          renderSectionHeader={({section}) => {
-            // console.log(section)
-            return (
-              <Text style = {styles.sectionHeaderText}>{section.type}</Text>
-            )
-          }}
-
-          ItemSeparatorComponent={() => (<View style = {{height: 16}}></View>)}
-          SectionSeparatorComponent={() => (<View style = {{height: 16}}></View>)}
+    <KeyboardAvoidingView behavior='padding' keyboardVerticalOffset={Platform.OS === "ios" ? 100 : 0} style = {styles.container}>
+      <View style = {styles.form}>
+        <Image source={require("./assets/adaptive-icon.png")} style = {styles.image}/>
+        <Text style = {styles.label}>Username</Text>
+        <TextInput 
+          style = {styles.input} 
+          placeholder ="Enter your name"
+          value = {username}
+          onChangeText={setUsername}
         />
+        {
+          errors.username ? (<Text style = {styles.errorText}>{errors.username}</Text>) : null
+        }
+        <Text style = {styles.label}>Password</Text>
+        <TextInput 
+          style = {styles.input} 
+          placeholder ="Enter your password" 
+          secureTextEntry
+          value = {password}
+          onChangeText={setPassword}
+        />
+        {
+          errors.password ? (<Text style = {styles.errorText}>{errors.password}</Text>) : null
+        }
+        <Button title='Login' onPress={handleForm} ></Button>
       </View>
-      
-    </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -68,40 +58,44 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#f5f5f5",
-    paddingTop: StatusBar.currentHeight,
+    justifyContent: "center",
+    paddingHorizontal: 20
   },
 
-  scrollView: {
-    paddingHorizontal: 16,
-    justifyContent:"center"
+  form: {
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    elevation: 5,
+    shadowColor: "#333",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.4,
+    shadowRadius: 4
   },
-
-  card: {
-    backgroundColor: "white",
-    padding: 16,
-    borderRadius: 8,
-    borderWidth: 2,
-    // marginBottom: 16,
-    elevation: 5
+  label: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 10
   },
-
-  cardText: {
-    fontSize: 30
+  input: {
+    height: 40,
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 10
   },
-
-  headerList: {
-    fontSize: 24,
-    textAlign: "center",
-    marginBottom: 12
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 50,
+    alignSelf: 'center'
   },
-
-  footerList: {
-    fontSize: 24,
-    textAlign: "center",
-    marginTop: 12
-  },
-
-  sectionHeaderText: {
-    fontSize: 24,
+  errorText: {
+    color:"red",
+    marginBottom: 10,
   }
 });
