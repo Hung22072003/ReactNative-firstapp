208eb727a227a8f51aef567f7316172f98caf5f8
Hung22072003 <tranngocnhathung2207@gmail.com>
2024-04-23 19:51:04 +0700

    add React Native List

--- App.js
+++ App.js
@@ -1,51 +1,65 @@
-import { StyleSheet, SafeAreaView, Platform, ScrollView } from 'react-native';
-import PokemonCard from './components/PokemonCard';
-
+import { StyleSheet, SafeAreaView, Platform, ScrollView, View, Text, StatusBar, FlatList, SectionList } from 'react-native';
+import pokemonList from "./data.json"
+import groupedPokemonList from "./grouped-data.json"
 export default function App() {
-  const charmanderData = {
-    name: "Charmander",
-    image: require("./assets/charmander.png"),
-    type: "Fire",
-    hp: 39,
-    moves: ["Scratch", "Ember", "Growl", "Leer"],
-    weaknesses:["Water", "Rock"]
-  }
-
-  const squirtleData = {
-    name: "Squirtle",
-    image: require("./assets/squirtle.png"), // Replace with the actual image path
-    type: "Water",
-    hp: 44,
-    moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
-    weaknesses: ["Electric", "Grass"],
-  };
-
-  const bulbasaurData = {
-    name: "Bulbasaur",
-    image: require("./assets/bulbasaur.png"), // Replace with the actual image path
-    type: "Grass",
-    hp: 45,
-    moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
-    weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
-  };
-
-  const pikachuData = {
-    name: "Pikachu",
-    image: require("./assets/pikachu.png"), // Replace with the actual image path
-    type: "Electric",
-    hp: 35,
-    moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
-    weaknesses: ["Ground"],
-  };
-
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <PokemonCard {...charmanderData} />
-        <PokemonCard {...squirtleData} />
-        <PokemonCard {...bulbasaurData} />
-        <PokemonCard {...pikachuData} />
-      </ScrollView>
+    <SafeAreaView style = {styles.container}>
+      {/* <ScrollView style = {styles.scrollView}>
+        {
+          pokemonList.map((pokemon) => {
+            return (
+              <View style = {styles.card} key = {pokemon.id}>
+                <Text style = {styles.cardText}>{pokemon.type}</Text>
+                <Text style = {styles.cardText}>{pokemon.name}</Text>
+              </View>
+            );
+          })
+        }
+      </ScrollView> */}
+
+      <View style = {styles.scrollView}>
+        {/* <FlatList 
+          data = {pokemonList}
+          renderItem={({item}) => {
+            // console.log(item.id)
+            return (
+              <View style = {styles.card} key = {item.id}>
+                <Text style = {styles.cardText}>{item.type}</Text>
+                <Text style = {styles.cardText}>{item.name}</Text>
+              </View>
+            );
+          }}
+
+          keyExtractor={(item, index) => item.id.toString()}
+          ItemSeparatorComponent={<View style = {{height: 16}}></View>}
+          ListEmptyComponent={<Text>No items found</Text>}
+          ListHeaderComponent={<Text style = {styles.headerList}>Pokemon List</Text>}
+          ListFooterComponent={<Text style = {styles.footerList}>End of List</Text>}
+        /> */}
+
+        <SectionList 
+          sections={groupedPokemonList}
+          renderItem={({item}) => {
+            // console.log(item)
+            return (
+              <View style = {styles.card}>
+                <Text style = {styles.cardText}>{item}</Text>
+              </View>
+            )
+          }}
+
+          renderSectionHeader={({section}) => {
+            // console.log(section)
+            return (
+              <Text style = {styles.sectionHeaderText}>{section.type}</Text>
+            )
+          }}
+
+          ItemSeparatorComponent={() => (<View style = {{height: 16}}></View>)}
+          SectionSeparatorComponent={() => (<View style = {{height: 16}}></View>)}
+        />
+      </View>
+      
     </SafeAreaView>
   );
 }
@@ -54,6 +68,40 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#f5f5f5",
-    paddingTop: Platform.OS === "android" ? 40 : 0,
+    paddingTop: StatusBar.currentHeight,
+  },
+
+  scrollView: {
+    paddingHorizontal: 16,
+    justifyContent:"center"
+  },
+
+  card: {
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 2,
+    // marginBottom: 16,
+    elevation: 5
+  },
+
+  cardText: {
+    fontSize: 30
+  },
+
+  headerList: {
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 12
+  },
+
+  footerList: {
+    fontSize: 24,
+    textAlign: "center",
+    marginTop: 12
+  },
+
+  sectionHeaderText: {
+    fontSize: 24,
   }
 });
